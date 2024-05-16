546d3fefd40ec75fe96ecc619f472fd5a6b8c109
Ayama11 <ayamalla882@gmail.com>
2024-05-16 14:42:06 +0300

    Add custom card widget for cart items
    Added a new custom card widget to display individual cart items with
    relevant information and icons for quantity adjustment. This enhances the
    visual representation of items in the cart.

--- lib/feature/cart/presintions/views/widgets/cart_body.dart
+++ lib/feature/cart/presintions/views/widgets/cart_body.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:testptc/core/utils/assets.dart';
 import 'package:testptc/core/utils/consts.dart';
+import 'package:testptc/feature/cart/presintions/views/widgets/custom_card.dart';
+import 'package:testptc/feature/cart/presintions/views/widgets/icons.dart';
+import 'package:testptc/feature/home/presintions/views/widgets/grid_home.dart';
 
 class CartBody extends StatelessWidget {
   const CartBody({super.key});
-
+  final String s = "\$";
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -18,17 +22,12 @@ class CartBody extends StatelessWidget {
           style: Styles.textStyle16W400
               .copyWith(fontWeight: FontWeight.w600, color: Colors.black),
         ),
-        const SizedBox(height: 32),
-        Card(
-            margin: const EdgeInsets.all(8),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: Row(
-              children: [
-                Image.asset(Assets.cart, height: 64, width: 70),
-              ],
-            )),
+        GridViewhome(
+            itemCount: 8,
+            itemBuilder: (BuildContext, int) {
+              return CustomCard(s: s);
+            },
+            crossAxisCount: 1),
       ],
     ));
   }
---
+++ lib/feature/cart/presintions/views/widgets/custom_card.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:testptc/core/utils/assets.dart';
+import 'package:testptc/core/utils/consts.dart';
+import 'package:testptc/feature/cart/presintions/views/widgets/icons.dart';
+
+class CustomCard extends StatelessWidget {
+  const CustomCard({
+    super.key,
+    required this.s,
+  });
+
+  final String s;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        color: Colors.white,
+        margin: const EdgeInsets.all(8),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(25),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(Assets.cart, height: 64, width: 70),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Organic Bananas",
+                    style: Styles.textStyle16W400.copyWith(color: Colors.black),
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    "1kg, Price",
+                    style: Styles.textStyle16W400.copyWith(
+                        color: const Color.fromARGB(255, 107, 107, 107)),
+                  ),
+                  const SizedBox(height: 10),
+                  const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CircularIcons(
+                          height: 40,
+                          width: 40,
+                          size: 24,
+                          icon: Iconsax.add,
+                          color: Consts.kPraimaryColor,
+                          backgrounColor: Color.fromARGB(255, 238, 236, 236),
+                        ),
+                        SizedBox(width: 10),
+                        Text("5"),
+                        SizedBox(width: 10),
+                        CircularIcons(
+                          backgrounColor: Color.fromARGB(255, 238, 236, 236),
+                          width: 40,
+                          height: 40,
+                          size: 24,
+                          color: Colors.grey,
+                          icon: Iconsax.minus,
+                        ),
+                      ]),
+                ],
+              ),
+              Column(
+                children: [
+                  const Icon(Icons.close),
+                  const SizedBox(height: 45),
+                  Text(
+                    "66 $s",
+                    style: Styles.textStyle16W400.copyWith(
+                        color: Colors.black, fontWeight: FontWeight.w600),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ));
+  }
+}
